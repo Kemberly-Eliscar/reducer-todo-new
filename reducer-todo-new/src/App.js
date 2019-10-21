fdb29e5bb1ce8ef57c174c48fbae557229fe2e3f
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-10-20 20:36:36 -0400

    reducers are working

--- reducer-todo-new/src/App.js
+++ reducer-todo-new/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+import {reducer, initialState} from "./reducers/reducer";
+import './to_do_list.scss'
+//import 'semantic-ui-css/semantic.min/css'
+
+import ToDoCard from './components/ToDoCard'
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ToDoCard state = {state} dispatch = {dispatch} />
+     
   );
 }
 
